1126b3add904cea191c83091ff254d6f7e3776ae
Datthehooman <datbui13579@gmail.com>
2026-01-06 23:33:37 +0700

    Add new image to the project and new tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,47 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./js/**/*.js"],
+  content: ["./index.html", "./**/*.js"],
   theme: {
     extend: {
       colors: {
-        // Add custom colors here if needed
+        primary: "#000000",
+        accent: "#d5416e",
+
+        pink: {
+          soft: "#fff7f8",
+        },
+
+        gray: {
+          light: "#f7f7f7",
+          muted: "#737373",
+        },
+
+        success: "#0c7c00",
+        danger: "#a60003",
       },
+
       fontFamily: {
-        sans: ["Nunito", "system-ui", "sans-serif"],
-        serif: ["Lora", "serif"],
+        heading: ["Lora", "serif"],
+        body: ["Nunito", "sans-serif"],
         accent: ["Trirong", "serif"],
         ui: ["Montserrat", "sans-serif"],
       },
-      spacing: {
-        // Add custom spacing values here if needed
+
+      fontSize: {
+        xs: ["12px", { lineHeight: "1.3" }],
+        sm: ["14px", { lineHeight: "1.3" }],
+        base: ["16px", { lineHeight: "1.3" }],
+        lg: ["18px", { lineHeight: "1.3" }],
+
+        h3: ["24px", { lineHeight: "1.3", fontWeight: "400" }],
+        h2: ["28px", { lineHeight: "1.3", fontWeight: "400" }],
+        h1: ["32px", { lineHeight: "1.3", fontWeight: "400" }],
+      },
+
+      borderRadius: {
+        lg: "8px",
+        xl: "12px",
+        pill: "32px",
       },
     },
   },
